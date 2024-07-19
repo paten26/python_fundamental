71006fcb99c152c596c9205d363c037db3082e36
LUKAS YULIANTO <paten26@gmail.com>
2024-07-19 16:53:30 +0700

    sintaks dasar

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+SINTAKSIS DASAR
+"""
+
+#1.SEKUENSIAL
+print("Dia levi")
+print('Aku siapa?')
+
+#2.PERCABANGAN
+#Contoh soal : levi menyuruh saya membeli kopi, jika harga 1 gelas kopi Rp.5000, beli 2 gelas. jika lebih beli 1 gelas saja
+harga_kopi = 6000
+if harga_kopi == 5000:
+    print("beli 2 gelas")
+else:
+    print("beli 1 gelas")
