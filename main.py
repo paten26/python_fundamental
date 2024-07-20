eddebcb633227f8fcd435563af589dbcd3958a07
LUKAS YULIANTO <paten26@gmail.com>
2024-07-20 21:45:47 +0700

    sintaks dasar

--- main.py
+++ main.py
@@ -28,12 +28,22 @@ for Jumlah_kopi_sudah_diminum in range (1, Jumlah_kopi+1):
     print(f"kopi yang sudah diminum = {Jumlah_kopi_sudah_diminum} gelas")
 print("")
 
-#PENGULANGAN WHILE
-print("CONTOH PENGULANGAN DENGAN WHILE")
+#PENGULANGAN WHILE 1
+print("CONTOH PENGULANGAN DENGAN WHILE 1")
 #Contoh soal : Levi menyuruh saya meminum seluruh kopi yang ada
 Jumlah_kopi = 10
-Jumlah_kopi_sudah_diminum = 100
+Jumlah_kopi_sudah_diminum = 0
 
 while Jumlah_kopi_sudah_diminum < Jumlah_kopi :
     Jumlah_kopi_sudah_diminum = Jumlah_kopi_sudah_diminum + 1
     print(f"kopi yang sudah diminum {Jumlah_kopi_sudah_diminum} gelas")
+print("")
+
+#PENGULANGAN WHILE 2
+print("CONTOH PENGULANGAN DENGAN WHILE 2")
+#Contoh soal : Levi menyuruh saya meminum seluruh kopi yang ada sampai habis
+Jumlah_kopi = 10
+Jumlah_kopi_sudah_diminum_dan_habis = 10
+
+while Jumlah_kopi_sudah_diminum_dan_habis == Jumlah_kopi_sudah_diminum_dan_habis + 10:
+    print("semua kopi sudah diminum dan habis")
