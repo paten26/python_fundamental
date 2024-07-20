b3f850f669a25bce2b60ddaf73f2b5793c74932c
LUKAS YULIANTO <paten26@gmail.com>
2024-07-20 20:51:00 +0700

    sintaks dasar

--- main.py
+++ main.py
@@ -3,21 +3,37 @@ SINTAKSIS DASAR
 """
 
 #1.SEKUENSIAL
+print("CONTOH SEKUENSIAL")
 print("Dia levi")
 print('Aku siapa?')
+print("")
 
 #2.PERCABANGAN
+print("CONTOH PERCABANGAN IF ELSE")
 #Contoh soal : levi menyuruh saya membeli kopi, jika harga 1 gelas kopi Rp.5000, beli 2 gelas. jika lebih beli 1 gelas saja
 harga_kopi = 6000
 if harga_kopi == 5000:
     print("beli 2 gelas")
 else:
     print("beli 1 gelas")
+print("")
 
-#3.PENGULANGAN
+#3.PENGULANGAN FOR
+print("CONTOH PENGULANGAN DENGAN FOR")
 #Contoh soal : Levi menyuruh saya meminum seluruh kopi yang ada
 Jumlah_kopi = 10
 Jumlah_kopi_sudah_diminum = 0
 
 for Jumlah_kopi_sudah_diminum in range (1, Jumlah_kopi+1):
     print(f"kopi yang sudah diminum = {Jumlah_kopi_sudah_diminum} gelas")
+print("")
+
+#PENGULANGAN WHILE
+print("CONTOH PENGULANGAN DENGAN WHILE")
+#Contoh soal : Levi menyuruh saya meminum seluruh kopi yang ada
+Jumlah_kopi = 10
+Jumlah_kopi_sudah_diminum = 100
+
+while Jumlah_kopi_sudah_diminum < Jumlah_kopi :
+    Jumlah_kopi_sudah_diminum = Jumlah_kopi_sudah_diminum + 1
+    print(f"kopi yang sudah diminum {Jumlah_kopi_sudah_diminum} gelas")
