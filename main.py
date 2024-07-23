67d699e5fa7e78ceb53d38e65ad4f1d0503b52fd
LUKAS YULIANTO <paten26@gmail.com>
2024-07-23 21:20:49 +0700

    sintaks dasar

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ print('Aku siapa?')
 print("")
 
 #2.PERCABANGAN
-print("CONTOH PERCABANGAN IF ELSE")
+print("\nCONTOH PERCABANGAN IF ELSE") #\n sebagai enter memberikan spasi saat run
 #Contoh soal : levi menyuruh saya membeli kopi, jika harga 1 gelas kopi Rp.5000, beli 2 gelas. jika lebih beli 1 gelas saja
 harga_kopi = 6000
-if harga_kopi == 5000:
+if harga_kopi == 5000: #if (jika), : (maka)
     print("beli 2 gelas")
 else:
     print("beli 1 gelas")
 print("")
 
 #3.PENGULANGAN FOR
-print("CONTOH PENGULANGAN DENGAN FOR")
+print("\nCONTOH PENGULANGAN DENGAN FOR")
 #Contoh soal : Levi menyuruh saya meminum seluruh kopi yang ada
 Jumlah_kopi = 10
 Jumlah_kopi_sudah_diminum = 0
@@ -29,21 +29,25 @@ for Jumlah_kopi_sudah_diminum in range (1, Jumlah_kopi+1):
 print("")
 
 #PENGULANGAN WHILE 1
-print("CONTOH PENGULANGAN DENGAN WHILE 1")
+print("\nCONTOH PENGULANGAN DENGAN WHILE 1")
 #Contoh soal : Levi menyuruh saya meminum seluruh kopi yang ada
 Jumlah_kopi = 10
 Jumlah_kopi_sudah_diminum = 0
 
-while Jumlah_kopi_sudah_diminum < Jumlah_kopi :
+while Jumlah_kopi_sudah_diminum < Jumlah_kopi : #while (selama)
     Jumlah_kopi_sudah_diminum = Jumlah_kopi_sudah_diminum + 1
     print(f"kopi yang sudah diminum {Jumlah_kopi_sudah_diminum} gelas")
-print("")
 
 #PENGULANGAN WHILE 2
-print("CONTOH PENGULANGAN DENGAN WHILE 2")
+print("\nCONTOH PENGULANGAN DENGAN WHILE 2")
 #Contoh soal : Levi menyuruh saya meminum seluruh kopi yang ada sampai habis
 Jumlah_kopi = 10
 Jumlah_kopi_sudah_diminum_dan_habis = 10
 
-while Jumlah_kopi_sudah_diminum_dan_habis == Jumlah_kopi_sudah_diminum_dan_habis + 10:
-    print("semua kopi sudah diminum dan habis")
+while Jumlah_kopi_sudah_diminum_dan_habis < Jumlah_kopi :
+    Jumlah_kopi_sudah_diminum_dan_habis = Jumlah_kopi_sudah_diminum_dan_habis + 1
+    if Jumlah_kopi_sudah_diminum_dan_habis == 0:
+        print(f"Kopi ke {Jumlah_kopi_sudah_diminum_dan_habis} belum habis")
+    else:
+        Jumlah_kopi_sudah_diminum_dan_habis = Jumlah_kopi_sudah_diminum_dan_habis + 1
+        print(f"Kopi ke {Jumlah_kopi_sudah_diminum_dan_habis} sudah diminum dan habis")
